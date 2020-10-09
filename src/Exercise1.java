14cd9f86277f7ffdb01f5aca9dd26a42ff176001
Lucas Osiecki <LOsiecki@ucvts.tec.nj.us>
2020-10-09 11:10:34 -0400

    started

--- src/Exercise1.java
+++ src/Exercise1.java
@@ -1,1 +1,18 @@
 package src;
+
+import java.util.Scanner;
+
+public class Exercise1 {
+    public static void main (String[] args) {
+
+        Scanner keyboard = new Scanner(System.in);
+
+
+        System.out.print("Enter an integer: ");
+        double integer = keyboard.nextDouble();
+
+        if (integer > 0){
+            
+        }
+    }
+}
